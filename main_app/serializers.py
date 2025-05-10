36ff913274209d964d90914bc25bff67c5c84346
User Name <victoriamurra30@gmail.com>
2025-05-10 14:11:58 -0400

    serializers

--- main_app/serializers.py
+++ main_app/serializers.py
@@ -1,15 +1,42 @@
 from rest_framework import serializers
-from .models import User, Pets, Shelter, AdoptionInquiry
-
+from django.contrib.auth.models import User
+from .models import Pet, Shelter, AdoptionInquiry
 
 class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=true)
+    password = serializers.CharField(write_only=True)
 
-    class meta:
+    class Meta:
         model = User
-        fields = ("id", "username", "email")
+        fields = ('id', 'username', 'email', 'password')
 
     def create(self, validated_data):
         user = User.objects.create_user(
-            username=validated_data["username"], email=validated_data
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
         )
+        return user
+
+
+class ShelterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Shelter
+        fields = '__all__'
+
+
+class PetSerializer(serializers.ModelSerializer):
+    shelter = ShelterSerializer(read_only=True)
+
+    class Meta:
+        model = Pet
+        fields = '__all__'
+
+
+class AdoptionInquirySerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(read_only=True)
+    pet = PetSerializer(read_only=True)
+
+    class Meta:
+        model = AdoptionInquiry
+        fields = '__all__'
+        read_only_fields = ('user', 'pet', 'created_at')
